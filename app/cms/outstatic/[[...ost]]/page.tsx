6f34d26b772989304bc0cbbbe8a3291af9dbf88f
Brittny Lapierre <brittny.lapierre@gmail.com>
2026-07-17 12:06:31 -0400

    feat: add outstatic dashboard and api routes

---
+++ app/cms/outstatic/[[...ost]]/page.tsx
@@ -0,0 +1,9 @@
+import 'outstatic/outstatic.css'
+import { Outstatic } from 'outstatic'
+import { OstClient } from 'outstatic/client'
+
+export default async function Page({ params }: { params: Promise<{ ost?: string[] }> }) {
+  const ostData = await Outstatic()
+  const resolvedParams = await params
+  return 
+}
